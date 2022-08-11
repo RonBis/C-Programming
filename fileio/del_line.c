aa4d02f484d48abca0309caf84d5b83160097978
RonBis <ronilbiswas.8937@gmail.com>
2022-08-11 21:43:22 +0530

    Changes to be committed: 	modified:   fileio/del_line.c

--- fileio/del_line.c
+++ fileio/del_line.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-char *read_string() {
+char *read_string(FILE *src_stream, char *prompt) {
   int str_len = 0;
   char *str, ch;
+  char stream_terminator = src_stream == stdin ? '\n' : EOF;
 
   str = malloc(sizeof(char));
 
-  while ((ch = getchar()) != '\n') {
+  if (prompt != NULL) {
+    printf("%s", prompt);
+  }
+
+  while ((ch = getc(src_stream)) != stream_terminator) {
     // reallocate with size = str_len
     str = realloc(str, (str_len + 1) * sizeof(char));
     *(str + str_len) = ch;
@@ -23,14 +29,15 @@ char *read_string() {
 
 int main(void) {
   FILE *fp;
-  char *str_to_be_deleted, ch;
+  char *str_to_be_deleted, *fp_content;
 
-  // str_to_be_deleted = read_string();
-  fp = fopen("sample.txt", "w+");
+  str_to_be_deleted = read_string(stdin, "String to be deleted > ");
 
-  while((ch == getc(fp)) != EOF) {
-    printf("%c", ch);
-  }
+  fp = fopen("sample.txt", "r+");
+  fp_content = read_string(fp, NULL);
 
+  printf("%d %d\n", (int)strstr(fp_content, str_to_be_deleted), (int)fp_content);
+
+  fclose(fp);
   return 0;
 }
