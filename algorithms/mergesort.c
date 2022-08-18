3efe05d654bdad8c93299825dc731a3db40ee8a8
RonBis <ronilbiswas.8937@gmail.com>
2022-08-18 22:12:53 +0530

    update mergesort.c

--- algorithms/mergesort.c
+++ algorithms/mergesort.c
@@ -26,31 +26,30 @@ void merge(float arr[], int p, int mid_pos, int r)
       {
         arr_k[k] = arr1[i++];
       }
-      if (arr1[i] > arr2[j])
+      else if (arr1[i] >= arr2[j])
       {
         arr_k[k] = arr2[j++];
       }
     }
-
-    if (i == arr1_size && j != arr2_size)
+    else if (i == arr1_size && j != arr2_size)
     {
-      for (int m = arr1_size - 1; m < arr2_size; m++)
+      for (int m = j; m < arr2_size; m++)
       {
-        arr_k[i] = arr2[m];
+        arr_k[k] = arr2[m];
       }
     }
     else if (i != arr1_size && j == arr2_size)
     {
-      for (int m = arr2_size - 1; m < arr1_size; m++)
+      for (int m = i; m < arr1_size; m++)
       {
-        arr_k[i] = arr2[m];
+        arr_k[k] = arr1[m];
       }
     }
   }
 
   for (int i = 0; i < arr_ksize; i++)
   {
-    printf("%f\t", arr_k[i]);
+    printf("%.3f\n", arr_k[i]);
   }
 }
 
@@ -61,18 +60,16 @@ void mergesort(float arr[], int p, int r)
     int mid_pos = (p + r);
     mergesort(arr, p, mid_pos - 1);
     mergesort(arr, mid_pos, r);
-
-    printf("%d, %d, %d", p, mid_pos, r);
     merge(arr, p, mid_pos, r);
   }
 }
 
 int main()
 {
-  // float arr[7] = {2.355, 4.5, 3.6, 2.12, 3.6, 6.4, 2.355};
-  float arr[6] = {0, 10, 21, 8, 15, 20};
+  float arr[7] = {2.355, 4.5, 3.6, 2.12, 3.6, 6.4, 2.355};
+  // float arr[9] = {0, 10, 21, 47, 49, 8, 15, 20, 78};
 
-  // mergesort(arr, 0, 6);
-  merge(arr, 0, 2, 5);
+  mergesort(arr, 0, 6);
+  // merge(arr, 0, 4, 8);
   return 0;
 }
