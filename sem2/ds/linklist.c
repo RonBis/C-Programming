957492ca71319d564a4f198dbe870786818abc8b
RonBis <ronilbiswas.8937@gmail.com>
2023-05-01 21:55:42 +0530

    finish linklist.c

--- sem2/ds/linklist.c
+++ sem2/ds/linklist.c
@@ -5,12 +5,10 @@ typedef struct NODE {
   int data;
   struct NODE *next;
 } node;
-node *head = NULL;
 
-int count() {
-  int count = 0;
-
+int count(node *head) {
   node *tmp = head;
+  int count = 0;
   while (tmp != NULL) {
     tmp = tmp->next;
     count++;
@@ -27,15 +25,16 @@ node *createNode() {
   return nd;
 }
 
-void insertAtBeg() {
+node *insertAtBeg(node *head) {
   node *nd = createNode();
   nd->next = head;
   head = nd;
+  return head;
 }
 
-void insertAtEnd() {
+node *insertAtEnd(node *head) {
   if (head == NULL) {
-    insertAtBeg();
+    return insertAtBeg(head);
   } else {
     node *tmp = head;
     while (tmp->next != NULL) {
@@ -43,22 +42,23 @@ void insertAtEnd() {
     }
     tmp->next = createNode();
   }
+  return head;
 }
 
 /// 1 means first position
-void insertAtPos() {
+node *insertAtPos(node *head) {
   int pos;
   printf("\nEnter position for new node: ");
   scanf("%d", &pos);
 
-  int node_count = count();
+  int node_count = count(head);
   if (pos < 1 || pos > node_count + 1) {
     printf("Index out of bound.\n");
-    return;
+    return head;
   }
 
   if (pos == 1) {
-    insertAtBeg();
+    return insertAtBeg(head);
   } else {
     node *tmp = head;
     node *nd = createNode();
@@ -74,9 +74,10 @@ void insertAtPos() {
       curr++;
     }
   }
+  return head;
 }
 
-void display() {
+void display(node *head) {
   if (head == NULL) {
     printf("\nLinked list is empty.\n");
     return;
@@ -91,22 +92,23 @@ void display() {
   printf("\n");
 }
 
-void delFromStart() {
+node *delFromStart(node *head) {
   if (head == NULL) {
     printf("\nLinked list is empty.\n");
-    return;
+    return head;
   };
 
   node *firstNode = head;
   head = head->next;
   free(firstNode);
   printf("\n");
+  return head;
 }
 
-void delFromEnd() {
+node *delFromEnd(node *head) {
   if (head == NULL) {
     printf("\nLinked list is empty.\n");
-    return;
+    return head;
   };
 
   node *cur = head, *prev = head;
@@ -117,7 +119,7 @@ void delFromEnd() {
       free(lastNode);
       if (cur == head) {
         head = NULL;
-        return;
+        return head;
       }
       prev->next = NULL;
       break;
@@ -128,21 +130,22 @@ void delFromEnd() {
   }
 
   printf("\n");
+  return head;
 }
 
-void delFromPos() {
+node *delFromPos(node *head) {
   int pos;
   printf("\nEnter position for deleting node: ");
   scanf("%d", &pos);
 
-  int nodeCount = count();
+  int nodeCount = count(head);
   if (pos < 1 || pos > nodeCount) {
     printf("Given position is out of bound.\n");
-    return;
+    return head;
   }
 
   if (pos == 1) {
-    return delFromStart();
+    return delFromStart(head);
   }
 
   node *temp = head;
@@ -155,10 +158,102 @@ void delFromPos() {
   node *loc = temp->next;
   temp->next = temp->next->next;
   free(loc);
+
+  return head;
+}
+
+void concatenate() {
+  printf("\nEnter first linklist elements:");
+  node *head1 = NULL;
+  char ch = 'y';
+  while (1) {
+    head1 = insertAtEnd(head1);
+
+    printf("Add elements (y/n)? ");
+    scanf(" %c", &ch);
+    if (ch == 'y')
+      continue;
+    break;
+  }
+
+  node *tail = head1;
+  while (tail->next != NULL) {
+    tail = tail->next;
+  }
+
+  printf("\nEnter second linklist elements:");
+  node *head2 = NULL;
+  while (1) {
+    head2 = insertAtEnd(head2);
+
+    printf("Add elements (y/n)? ");
+    scanf(" %c", &ch);
+    if (ch == 'y')
+      continue;
+    break;
+  }
+
+  tail->next = head2;
+
+  printf("\nMerged linklist:");
+  display(head1);
+
+  free(head1);
+}
+
+void search(node *head) {
+  if (head == NULL) {
+    printf("\nLinked list is empty.\n");
+    return;
+  }
+
+  int data;
+  printf("\nEnter data to be searched: ");
+  scanf("%d", &data);
+  int pos = -1;
+
+  int counter = 1;
+  node *tmp = head;
+  while (tmp != NULL) {
+    if (tmp->data == data) {
+      printf("Found at position: %d\n", counter);
+      pos = counter;
+      break;
+    }
+    tmp = tmp->next;
+    counter++;
+  }
+  if (pos == -1) {
+    printf("Item not found.\n");
+  }
+}
+
+node *reverseLinklist(node *head) {
+  if (head == NULL)
+    return head;
+
+  // allocate mem for no of nodes in linkedlist
+  int counter;
+  int nodeCount = counter = count(head);
+  node *arr[nodeCount], *tmp = head;
+
+  while (tmp != NULL) {
+    arr[--counter] = tmp;
+    tmp = tmp->next;
+  }
+
+  head = arr[0];
+  while (counter < nodeCount - 1) {
+    arr[counter]->next = arr[counter + 1];
+    counter++;
+  }
+  arr[counter]->next = NULL;
+  return head;
 }
 
 int main() {
   int choice;
+  node *head = NULL;
 
   while (1) {
     printf("\nEnter choice:\n"
@@ -166,55 +261,63 @@ int main() {
            "2. Insert at end\n"
            "3. Insert at pos\n"
            "4. Display\n"
-           "5. Count nodes\n"
-           "6. Delete from start\n"
-           "7. Delete from end\n"
-           "8. Delete at pos\n"
-           "9. Concatenate another linked list\n"
-           "10. Search element\n"
-           "11. Reverse linked list\n"
-           "12. Exit\n");
+           "5. Delete from start\n"
+           "6. Delete from end\n"
+           "7. Delete at pos\n"
+           "8. Concatenate another linked list\n"
+           "9. Search element\n"
+           "10. Reverse linked list\n"
+           "11. Exit\n");
     scanf("%d", &choice);
 
     switch (choice) {
     case 1:
       printf("--Insert at beginning--");
-      insertAtBeg();
+      head = insertAtBeg(head);
       break;
     case 2:
       printf("--Insert at end--");
-      insertAtEnd();
+      head = insertAtEnd(head);
       break;
     case 3:
       printf("--Insert at position--");
-      insertAtPos();
+      head = insertAtPos(head);
       break;
     case 4:
       printf("--Display--");
-      display();
+      display(head);
       break;
     case 5:
-      printf("--Count--");
-      printf("\nNode count: %d\n", count());
+      printf("--Delete from beginning--");
+      head = delFromStart(head);
       break;
     case 6:
-      printf("--Delete from beginning--");
-      delFromStart();
+      printf("--Delete from end--");
+      head = delFromEnd(head);
       break;
     case 7:
-      printf("--Delete from end--");
-      delFromEnd();
+      printf("--Delete from pos--");
+      head = delFromPos(head);
       break;
     case 8:
-      printf("--Delete from pos--");
-      delFromPos();
+      printf("--Concatenate two linklist--");
+      concatenate();
       break;
-    case 12:
+    case 9:
+      printf("--Search element--");
+      search(head);
+      break;
+    case 10:
+      printf("--Reverse linklist--");
+      head = reverseLinklist(head);
+      break;
+    case 11:
       exit(0);
     default:
       printf("Invalid option.\n");
     }
   }
 
+  free(head);
   return 0;
 }
