3135aad69da836feee2d58bc7ba2f9b28231b3f4
Ronil Biswas <66785801+RonBis@users.noreply.github.com>
2023-10-10 11:49:16 +0530

    Add files via upload

---
+++ sem3/lagrange_interpolation.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+
+double lagrange_interpolate(double x, int size, double table[size][2]) {
+	double res = 0;
+	for(int i=0; i<size; i++) {
+		double x_const = table[i][0];
+		double y = table[i][1];
+		
+		printf("\nx_const: %lf, y: %lf\t", x_const, y);
+		
+		double prod = y;
+		for(int j=0; j<size; j++) {
+			if(j == i) continue;
+			
+			prod *= ( x - table[j][0] )/( x_const - table[j][0] );
+		}
+		
+		res += prod;
+	}
+}
+
+int main() {
+	int n;
+	printf("Number of x inputs: ");
+	scanf("%d", &n);
+	
+	double table[n][2];
+	
+	// input x
+	for(int i=0; i<n; i++) {
+		printf("Input x[%d]> ", i);
+		scanf("%lf", &table[i][0]);
+	}
+	
+	printf("\n");
+	// input f(x) ie: y
+	for(int i=0; i<n; i++) {
+		printf("Input y[%d]> ", i);
+		scanf("%lf", &table[i][1]);
+	}
+	
+	double x;
+	printf("\nEnter value for lagrange interpolation: ");
+	scanf("%lf", &x);
+	
+	// interpolation
+	x = lagrange_interpolate(x, n, table);
+	printf("\nResult: %lf\n", x);
+	
+	return 0;
+}
+
